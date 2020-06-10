a9315973a0c864c1a9ed02f91953e1f650d5ebf9
developer01189998819991197253 <52958809+developer01189998819991197253@users.noreply.github.com>
2020-06-10 17:32:53 +0100

    Pal 653 eureka beans (#27)

--- clients-simple/src/main/java/uk/gov/gchq/palisade/clients/simpleclient/client/SimpleClient.java
+++ clients-simple/src/main/java/uk/gov/gchq/palisade/clients/simpleclient/client/SimpleClient.java
@@ -16,19 +16,15 @@
 
 package uk.gov.gchq.palisade.clients.simpleclient.client;
 
-import com.netflix.discovery.EurekaClient;
-import com.netflix.discovery.shared.Application;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.netflix.eureka.EurekaServiceInstance;
 
 import uk.gov.gchq.palisade.Context;
 import uk.gov.gchq.palisade.RequestId;
 import uk.gov.gchq.palisade.UserId;
 import uk.gov.gchq.palisade.clients.simpleclient.request.ReadRequest;
 import uk.gov.gchq.palisade.clients.simpleclient.request.RegisterDataRequest;
-import uk.gov.gchq.palisade.clients.simpleclient.web.DataClient;
+import uk.gov.gchq.palisade.clients.simpleclient.web.DataClientFactory;
 import uk.gov.gchq.palisade.clients.simpleclient.web.PalisadeClient;
 import uk.gov.gchq.palisade.data.serialise.Serialiser;
 import uk.gov.gchq.palisade.resource.LeafResource;
@@ -37,54 +33,73 @@ import uk.gov.gchq.palisade.service.request.DataRequestResponse;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * The type Simple client.
+ *
+ * @param <T> the type parameter
+ */
 public class SimpleClient<T> {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleClient.class);
     private final Serialiser<T> serialiser;
 
-    final PalisadeClient palisadeClient;
-    final DataClient dataClient;
+    private final PalisadeClient palisadeClient;
+    private final DataClientFactory dataClientFactory;
 
-    final EurekaClient eurekaClient;
-
-    public SimpleClient(final Serialiser<T> serialiser, final PalisadeClient palisadeClient, final DataClient dataClient, final EurekaClient eurekaClient) {
+    /**
+     * Instantiates a new Simple client.
+     *
+     * @param serialiser        the serialiser
+     * @param palisadeClient    the palisade client
+     * @param dataClientFactory the data client factory
+     */
+    public SimpleClient(final Serialiser<T> serialiser, final PalisadeClient palisadeClient, final DataClientFactory dataClientFactory) {
         this.serialiser = serialiser;
         this.palisadeClient = palisadeClient;
-        this.dataClient = dataClient;
-        this.eurekaClient = eurekaClient;
+        this.dataClientFactory = dataClientFactory;
     }
 
-    public Stream<T> read(final String filename, final String resourceType, final String userId, final String purpose) throws IOException, URISyntaxException {
-        DataRequestResponse dataRequestResponse = makeRequest(filename, resourceType, userId, purpose);
-        Stream<T> objectStreams = getObjectStreams(dataRequestResponse);
-        return objectStreams;
+    /**
+     * Read stream.
+     *
+     * @param filename the filename
+     * @param userId   the user id
+     * @param purpose  the purpose
+     * @return a stream of getObjectStream
+     * @throws IOException the io exception
+     */
+    public Stream<T> read(final String filename, final String userId, final String purpose) throws IOException {
+        DataRequestResponse dataRequestResponse = makeRequest(filename, userId, purpose);
+        return getObjectStreams(dataRequestResponse);
     }
 
-    private DataRequestResponse makeRequest(final String fileName, final String resourceType, final String userId, final String purpose) {
+    private DataRequestResponse makeRequest(final String fileName, final String userId, final String purpose) {
         RegisterDataRequest dataRequest = new RegisterDataRequest().resourceId(fileName).userId(new UserId().id(userId)).context(new Context().purpose(purpose));
 
         // While there may be many palisade services, just use one
-        ServiceInstance palisadeService = getServiceInstances("palisade-service").get(0);
-        return palisadeClient.registerDataRequestSync(palisadeService.getUri(), dataRequest);
+        return palisadeClient.registerDataRequestSync(dataRequest);
     }
 
-    public Stream<T> getObjectStreams(final DataRequestResponse response) throws URISyntaxException, IOException {
+    /**
+     * For a given DataRequestResponse returns a stream of deserialised objects from the dataClient
+     *
+     * @param response the response
+     * @return the object streams
+     * @throws IOException the io exception
+     */
+    public Stream<T> getObjectStreams(final DataRequestResponse response) throws IOException {
         requireNonNull(response, "response");
 
         final List<Stream<T>> dataStreams = new ArrayList<>(response.getResources().size());
         for (final LeafResource resource : response.getResources()) {
             final ConnectionDetail connectionDetail = resource.getConnectionDetail();
-            final URI dataService = new URI(connectionDetail.createConnection());
             final RequestId uuid = response.getOriginalRequestId();
 
             final ReadRequest readRequest = new ReadRequest()
@@ -93,26 +108,20 @@ public class SimpleClient<T> {
             readRequest.setOriginalRequestId(uuid);
 
             LOGGER.info("Resource {} has DATA-SERVICE connection detail {}", resource.getId(), connectionDetail);
-            InputStream responseStream = dataClient.readChunked(dataService, readRequest).body().asInputStream();
+            DataClientFactory.DataClient dataClient = dataClientFactory.build(connectionDetail.createConnection());
+            InputStream responseStream = dataClient.readChunked(readRequest).body().asInputStream();
             Stream<T> dataStream = getSerialiser().deserialise(responseStream);
             dataStreams.add(dataStream);
         }
         return dataStreams.stream().flatMap(Function.identity());
     }
 
+    /**
+     * Gets serialiser.
+     *
+     * @return the serialiser
+     */
     public Serialiser<T> getSerialiser() {
         return serialiser;
     }
-
-
-    public List<ServiceInstance> getServiceInstances(final String serviceName) {
-        return eurekaClient.getApplications().getRegisteredApplications().stream()
-                .map(Application::getInstances)
-                .flatMap(List::stream)
-                .filter(instance -> instance.getAppName().equalsIgnoreCase(serviceName))
-                .peek(instance -> LOGGER.info("Discovered {} :: {}:{}/{} ({})", instance.getAppName(), instance.getIPAddr(), instance.getPort(), instance.getSecurePort(), instance.getStatus()))
-                .map(EurekaServiceInstance::new)
-                .collect(Collectors.toList());
-    }
-
 }
---
+++ clients-simple/src/main/java/uk/gov/gchq/palisade/clients/simpleclient/web/UrlDataClient.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2020 Crown Copyright
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.gov.gchq.palisade.clients.simpleclient.web;
+
+import org.springframework.cloud.openfeign.FeignClientBuilder;
+import org.springframework.context.ApplicationContext;
+
+import uk.gov.gchq.palisade.clients.simpleclient.config.ClientConfiguration;
+
+import java.util.Map;
+
+/**
+ * The type Url data client for when the profile is not Eureka.
+ */
+public class UrlDataClient implements DataClientFactory {
+
+    private final FeignClientBuilder feignClientBuilder;
+    private final ClientConfiguration clientConfiguration;
+
+    /**
+     * Instantiates a new Url data client with a Feign Builder of url serviceId i.e localhost:8085.
+     *
+     * @param appContext          the app context
+     * @param clientConfiguration the clientConfiguration
+     */
+    public UrlDataClient(final ApplicationContext appContext, final ClientConfiguration clientConfiguration) {
+        this.feignClientBuilder = new FeignClientBuilder(appContext);
+        this.clientConfiguration = clientConfiguration;
+    }
+
+    public DataClient build(final String serviceId) {
+        Map<String, String> dataServices = clientConfiguration.getClient();
+        return feignClientBuilder
+                .forType(DataClient.class, serviceId)
+                .url(dataServices.getOrDefault(serviceId, serviceId))
+                .build();
+    }
+}
