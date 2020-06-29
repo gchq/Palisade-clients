f451d2235b13caec3474b55add297a3e2c57fb94
Dev 930018 <dev930018@gmail.com>
2020-06-29 15:27:17 +0100

    PAL=232 documentation of public methods on simple client

--- clients-simple/src/main/java/uk/gov/gchq/palisade/clients/simpleclient/client/SimpleClient.java
+++ clients-simple/src/main/java/uk/gov/gchq/palisade/clients/simpleclient/client/SimpleClient.java
@@ -55,9 +55,9 @@ public class SimpleClient<T> {
     /**
      * Instantiates a new Simple client.
      *
-     * @param serialiser        the serialiser
-     * @param palisadeClient    the palisade client
-     * @param dataClientFactory the data client factory
+     * @param serialiser        the serialiser to use for deserialising all inbound data
+     * @param palisadeClient    the client to use for connecting to the palisade-service
+     * @param dataClientFactory a factory for clients for many data-services, depending on the returned connectionDetail
      */
     public SimpleClient(final Serialiser<T> serialiser, final PalisadeClient palisadeClient, final DataClientFactory dataClientFactory) {
         this.serialiser = serialiser;
@@ -66,33 +66,66 @@ public class SimpleClient<T> {
     }
 
     /**
-     * Read stream.
+     * Does a request to the palisade-service for the given resource id using the user id and purpose for a context.
+     * Then does a read of the data-service for all returned resources, and deserialises using the configured serialiser.
      *
-     * @param filename the filename
-     * @param userId   the user id
-     * @param purpose  the purpose
-     * @return a stream of getObjectStream
-     * @throws IOException the io exception
+     * @param resourceId the resource id
+     * @param userId     the user id
+     * @param purpose    the purpose
+     * @return a stream of objects of type T from the data-service
+     * @throws IOException if deserialisation of the data-service response failed
      */
-    public Stream<T> read(final String filename, final String userId, final String purpose) throws IOException {
-        DataRequestResponse dataRequestResponse = registerRequest(filename, userId, purpose);
+    public Stream<T> read(final String resourceId, final String userId, final String purpose) throws IOException {
+        DataRequestResponse dataRequestResponse = registerRequest(resourceId, userId, purpose);
         return readResponse(dataRequestResponse);
     }
 
-    public DataRequestResponse registerRequest(final String fileName, final String userId, final String purpose) {
-        RegisterDataRequest dataRequest = new RegisterDataRequest().resourceId(fileName).userId(new UserId().id(userId)).context(new Context().purpose(purpose));
+    /**
+     * Does a request to the palisade-service for the given resource id using the user id and purpose for a context.
+     *
+     * @param resourceId the resource id
+     * @param userId     the user id
+     * @param purpose    the purpose
+     * @return the {@link DataRequestResponse} from the palisade-service
+     * @throws IOException if deserialisation of the data-service response failed
+     */
+    public DataRequestResponse registerRequest(final String resourceId, final String userId, final String purpose) {
+        RegisterDataRequest dataRequest = new RegisterDataRequest().resourceId(resourceId).userId(new UserId().id(userId)).context(new Context().purpose(purpose));
         return registerRequest(dataRequest);
     }
 
+    /**
+     * Does a request to the palisade-service for the given {@link RegisterDataRequest} request.
+     * Provides more control over the context object than providing just the purpose.
+     *
+     * @param request the request
+     * @return the {@link DataRequestResponse} from the palisade-service
+     * @throws IOException if deserialisation of the data-service response failed
+     */
     public DataRequestResponse registerRequest(final RegisterDataRequest request) {
         return palisadeClient.registerDataRequestSync(request);
     }
 
+    /**
+     * Does a read of the data-service for the specified resources, and deserialises using the configured serialiser.
+     *
+     * @param token       the token returned from a previous {@link DataRequestResponse}
+     * @param resourceSet the set of resources to read (if permitted)
+     * @return a stream of objects of type T from the data-service
+     * @throws IOException if deserialisation of the data-service response failed
+     */
     public Stream<T> readResponse(final String token, final Set<LeafResource> resourceSet) throws IOException {
         DataRequestResponse requestResponse = new DataRequestResponse().resources(resourceSet).token(token);
         return readResponse(requestResponse);
     }
 
+    /**
+     * Does a read of the data-service for all returned resources, and deserialises using the configured serialiser.
+     *
+     * @param response the {@link DataRequestResponse} from a previous call to the palisade-service
+     * @return a stream of objects of type T from the data-service
+     * @throws IOException if deserialisation of the data-service response failed
+     */
     public Stream<T> readResponse(final DataRequestResponse response) throws IOException {
         final List<Stream<T>> dataStreams = new ArrayList<>(response.getResources().size());
         for (final LeafResource resource : response.getResources()) {
@@ -114,7 +147,7 @@ public class SimpleClient<T> {
     }
 
     /**
-     * Gets serialiser.
+     * Gets the serialiser used by this client for deserialising all inbound data.
      *
      * @return the serialiser
      */
